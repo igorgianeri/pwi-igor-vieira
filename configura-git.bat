3c58e105394b0e2bdf3c853b9594175ce4a2317a
Igor <gianeri.vieira@gmail.com>
2024-08-16 20:25:56 -0300

    Meu primeiro site

---
+++ configura-git.bat
@@ -0,0 +1,16 @@
+echo OFF
+
+echo -------------------------------------
+echo Configurar Nome de Usuario
+git config --global user.name Igor Vieira
+echo -------------------------------------
+echo ----- Configurar Conta de usuario----
+git config --global user.email gianeri.vieira@gmail.com
+echo -------------------------------------
+echo ----------------Clone----------------
+git clone https://github.com/igorgianeri/pwi-igor-vieira.git
+echo -------------------------------------
+echo ------------Config Atual-------------
+ls
+
+pause
